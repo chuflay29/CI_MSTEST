6684f151186f8d9f0bbe1bd29f00a59178e85d45
j <juliocadima29@gmail.com>
2025-03-05 21:11:12 -0400

    primer test

---
+++ Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace CI_MSTEST
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
